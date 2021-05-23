0496c2571002de1755f24c29da1aab2a9ce6c9ee
kirill22111333 <35836563+kirill22111333@users.noreply.github.com>
2021-05-23 23:35:19 +0300

    layouts

---
+++ layouts/footer.php
@@ -0,0 +1,6 @@
+<footer>
+        <a class="footer_a" href="#">Разработал Кирулл</a>
+    </footer>
+<script src="assets/js/script.js"></script>
+</body>
+</html>
---
+++ layouts/nav.php
@@ -0,0 +1,8 @@
+<header>
+    <a href="index.php"><h2>LOGO</h2></a>
+    <ul>
+        <li><a href="">Главная</a></li>
+        <li><a href="">О проекте</a></li>
+        <li><a href="">О нас</a></li>
+    </ul>
+</header>
